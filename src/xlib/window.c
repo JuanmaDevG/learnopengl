81c248da23bd61f74c417bce4b54045ffd2fd5e3
JuanmaDevG <juanmagonzalezmartinez@protonmail.com>
2025-08-22 21:24:54 +0200

    minimal modifications

--- src/xlib/window.c
+++ src/xlib/window.c
@@ -5,11 +5,12 @@ int main()
   Display* disp = XOpenDisplay(NULL);
   XSetWindowAttributes wattr;
   wattr.background_pixel = WhitePixel(disp, DefaultScreen(disp));
+  wattr.win_gravity = CenterGravity;
 
   Window w = XCreateWindow(
       disp, DefaultRootWindow(disp), 0, 0, 800, 600, 0,
       DefaultDepth(disp, DefaultScreen(disp)), InputOutput, CopyFromParent,
-      CWBackPixel, &wattr);
+      CWBackPixel | CWWinGravity, &wattr);
 
   XDestroyWindow(disp, w);
   XCloseDisplay(disp);
