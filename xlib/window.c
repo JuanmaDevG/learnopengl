261086367259ff2faf251b269dd29542a1d08f8c
JuanmaDevG <juanmagonzalezmartinez@protonmail.com>
2025-08-24 01:29:19 +0200

    Debbugging first window program because does not map by BadRequest

--- xlib/window.c
+++ xlib/window.c
@@ -1,20 +1,43 @@
 #include <X11/Xlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int xlib_status;
+#define ERR_BUF_LENGTH 256
+
+int xlib_status = 0;
+char err_buf[ERR_BUF_LENGTH];
 
 int main()
 {
   Display* disp = XOpenDisplay(NULL);
+  if(!disp) {
+    printf("I mean... the display is null man\n");
+    exit(1);
+  }
+
   XSetWindowAttributes wattr;
   wattr.background_pixel = WhitePixel(disp, DefaultScreen(disp));
-  wattr.win_gravity = CenterGravity;
 
+  /*
   Window w = XCreateWindow(
       disp, DefaultRootWindow(disp), 0, 0, 800, 600, 0,
       DefaultDepth(disp, DefaultScreen(disp)), InputOutput, CopyFromParent,
-      CWBackPixel | CWWinGravity, &wattr);
+      CWBackPixel, &wattr);
+  */
+  //TODO: debug, BadRequest being sent
+  Window w = XCreateSimpleWindow(
+      disp, DefaultRootWindow(disp), 0, 0, 800, 600, 0, 0,
+      BlackPixel(disp, DefaultScreen(disp)));
 
   xlib_status = XMapWindow(disp, w);
+  if(xlib_status) {
+    XGetErrorText(disp, xlib_status, err_buf, ERR_BUF_LENGTH);
+    printf("CANNOT MAP WINDOW: %s\n", err_buf);
+    XCloseDisplay(disp);
+    exit(1);
+  }
+  sleep(2);
 
   XDestroyWindow(disp, w);
   XCloseDisplay(disp);
