6f956cd7df13e7e34e99544b4f039fb17f3df230
JuanmaDevG <juanmagonzalezmartinez@protonmail.com>
2025-08-28 12:47:30 +0200

    Changed some filenames

--- makefile
+++ makefile
@@ -7,11 +7,14 @@ all: window window-info atoms
 window: $(XLIBDIR)/window.c
 	gcc $^ -lX11 -o $@
 
-window-info: $(XLIBDIR)/window-info.c
+attributes: $(XLIBDIR)/info.c
 	gcc $^ -lX11 -o $@
 
 atoms: $(XLIBDIR)/atoms.c
 	gcc $^ -lX11 -o $@
 
+properties: $(XLIBDIR)/properties.c
+	gcc $^ -lX11 -o $@
+
 clean:
-	rm window window-info atoms
+	rm window attributes atoms properties
--- xlib/properties.c
+++ xlib/properties.c
@@ -2,7 +2,6 @@
 #include <X11/Atoms.h>
 #include <stdio.h>
 
-
 /* NOTE:
  * Window properties ARE NOT THE SAME AS widow attributes, a property list for 
  * a window is defined by the client, otherwise window attributes are defined 
@@ -10,8 +9,15 @@
  * XWindowAttributes struct.
  *
  * Window properties have {name, type, value} and can be attached to a window.
+ *
+ * NOTE ABOUT XGetWindowProperty:
+ * The last parameter prop_return gets a double pointer because it NEEDS a
+ * reference to WRITE into the client pointer that will point to the array of 
+ * data.
  */
 
+
 int main() {
+  Display *disp = XOpenDisplay(NULL);
   return 0;
 }
