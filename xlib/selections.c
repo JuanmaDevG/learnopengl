d694fc2380e3f01994cadbcffd86180a72ebf5da
JuanmaDevG <juanmagonzalezmartinez@protonmail.com>
2025-08-31 20:09:55 +0200

    Making a multiprocess producer/consumer data streaming program to understand selections

--- xlib/selections.c
+++ xlib/selections.c
@@ -1,10 +1,58 @@
 #include <X11/Xlib.h>
 #include <Xatom.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.c>
 
-//TODO: selections may need two appliactions to practice communication
+Display *disp;
+Atom selection;
+const Atom s_type = XA_STRING;
+const char *s_name = "CUSTOM_TMP_BUFFER";
+Window s_owner;
+
+//NOTE:
+// This is an example of selections by using a producer/consumer model
+
+
+void producer()
+{
+  disp = XOpenDisplay(NULL);
+  do {
+    selection = XInternAtom(disp, s_name, True);
+    sleep(1);
+  } while(selection == None);
+  printf("Producer got the atom\n");
+
+  do {
+    s_owner = XGetSelectionOwner(disp, selection);
+    sleep(1);
+  } while(s_owner == None);
+  printf("Producer got the selection owner\n");
+
+
+  //TODO: continue
+
+  XCloseDisplay(disp);
+  exit(0);
+}
+
+void consumer()
+{
+  disp = XOpenDisplay(NULL);
+
+  XCloseDisplay(disp);
+  exit(0);
+}
 
 int main()
 {
+  pid_t child = fork();
+  if(child == (pid_t)-1) {
+    perror("Could not generate the child process\n");
+    return 1;
+  }
+
+  producer();
+  if(child == 0) consumer();
   return 0;
 }
