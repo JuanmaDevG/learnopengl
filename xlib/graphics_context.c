cf5e8fd6d321ea7eca1246840438ee3a772c794a
JuanmaDevG <juanmagonzalezmartinez@protonmail.com>
2025-09-05 14:19:45 +0200

    it works but overloads cpu and Xorg with excessive workload because of an infinite ConfigureNotify loop that needs to be solved

--- xlib/graphics_context.c
+++ xlib/graphics_context.c
@@ -14,6 +14,12 @@
  *     as our drawable and the source as the graphics object wanting to melt.
  * - plane_mask: what bit planes of a color is the function applied over.
  * - __param__
+ *
+ *
+ * NOTE:
+ * XConfigureWindow generates a ConfigureNotify event, if you receive
+ * ConfigureNotify events from your OS, better stop the infinite loop when
+ * possible.
  */
 
 static Display *disp;
@@ -27,7 +33,7 @@ static XSetWindowAttributes attr = {
   .backing_store = NotUseful,
   .save_under = False,
   .override_redirect = False,
-  .event_mask = KeyPressMask | ResizeRedirectMask | StructureNotifyMask
+  .event_mask = KeyPressMask | StructureNotifyMask
 };
 
 static XGCValues gcval = {
@@ -41,6 +47,8 @@ static XGCValues gcval = {
   .fill_rule = EvenOddRule
 };
 
+static XWindowChanges wch;
+
 
 int main()
 {
@@ -52,6 +60,8 @@ int main()
   attr.background_pixel = BlackPixel(disp, screen_num);
   attr.border_pixel = WhitePixel(disp, screen_num);
 
+  wch.width = 800;
+  wch.height = 600;
   Window w = XCreateWindow(disp, wroot, 0, 0, 800, 600, 3, 
       default_depth, InputOutput, CopyFromParent,
       CWBitGravity | CWBackingStore | CWSaveUnder | CWEventMask | 
@@ -63,32 +73,22 @@ int main()
       GCForeground | GCBackground | GCCapStyle | GCJoinStyle,
       &gcval);
 
-  XMapWindow(disp, w);
   XSetGraphicsExposures(disp, gc, True);
+  XMapWindow(disp, w);
   XEvent ev;
-  unsigned int dumb_mem, w_width, w_height;
   while(1)
   {
     XNextEvent(disp, &ev);
     XClearWindow(disp, w);
     switch(ev.type)
     {
-      case MapNotify:
-        //TODO: debug (mess up with ConfigureNotifyEvent)
-        printf("MapNotify\n");
-      case ResizeRequest:
-        printf("ResizeRequest\n");
-        XGetGeometry(disp, w, &wroot, &dumb_mem, &dumb_mem,
-            &w_width, &w_height, &dumb_mem, &dumb_mem);
-        printf("w_width: %u, w_height: %u\n", w_width, w_height); //REMOVE
-        triangle[0].x = w_width / 2;
-        triangle[0].y = (w_height / 2) - (w_height / 4);
-        triangle[1].x = (w_width / 2) - (w_width / 4);
-        triangle[1].y = (w_height / 2) + (w_height / 4);
-        triangle[2].x = (w_width / 2) + (w_width / 4);
-        triangle[2].y = (w_height / 2) + (w_height / 4);
-
-        XFillPolygon(disp, w, gc, triangle, 3, Convex, CoordModeOrigin);
+      case ConfigureNotify: // Because my tiling wm changes it
+        wch.x = ev.xconfigure.x;
+        wch.y = ev.xconfigure.y;
+        wch.width = ev.xconfigure.width;
+        wch.height = ev.xconfigure.height;
+        XConfigureWindow(disp, w, CWX | CWY | CWHeight | CWWidth, &wch);
+        //TODO: XConfigureWindow generates a ConfigureNotify loop
         break;
       case KeyPress:
         KeySym ks = XLookupKeysym(&ev.xkey, 0);
@@ -99,6 +99,13 @@ int main()
           return 0;
         }
     }
+    triangle[0].x = wch.width / 2;
+    triangle[0].y = (wch.height / 2) - (wch.height / 4);
+    triangle[1].x = (wch.width / 2) - (wch.width / 4);
+    triangle[1].y = (wch.height / 2) + (wch.height / 4);
+    triangle[2].x = (wch.width / 2) + (wch.width / 4);
+    triangle[2].y = (wch.height / 2) + (wch.height / 4);
+    XFillPolygon(disp, w, gc, triangle, 3, Convex, CoordModeOrigin);
     XFlush(disp);
   }
 }
