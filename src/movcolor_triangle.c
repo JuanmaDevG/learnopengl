5bca661d3bcc31e2970f677e842e6aafe7f05d52
JuanmaDevG <juanmagonzalezmartinez@protonmail.com>
2025-12-29 00:20:13 +0100

    Minimal changes

--- src/movcolor_triangle.c
+++ src/movcolor_triangle.c
@@ -8,5 +8,23 @@
 
 int main()
 {
+  const GLfloat triangle = {
+    -0.5f, 0.5f, 0.0f,
+    -1.0f, -0.5f, 0.0f,
+    0.0f, -0.5f, 0.0f
+  };
+
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  GLFWwindow* w = glfwCreateWindow(800, 600, "Triangle moving and blinking", NULL, NULL);
+
+  //TODO: do buffers and shaders stuff
+
+  //TODO: modify color and position by uniform
+
   return 0;
 }
