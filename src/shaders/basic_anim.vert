0df51d7c04b8701207d34cab32500117bb9b2245
JuanmaDevG <juanmagonzalezmartinez@protonmail.com>
2025-12-31 01:23:44 +0100

    Done with shaders and program work in progress

--- src/shaders/basic_anim.vert
+++ src/shaders/basic_anim.vert
@@ -6,5 +6,5 @@ uniform float u_time;
 
 void main()
 {
-  gl_Position = vec4(pos.x + (sin(u_time * 2.5) / 2.0f), pos.yz, 1.0f);
+  gl_Position = vec4(pos.x + (sin(u_time * 2.5f) / 2.0f), pos.yz, 1.0f);
 }
--- src/tricolored_triangle.c
+++ src/tricolored_triangle.c
@@ -7,5 +7,50 @@
 
 int main()
 {
+  const GLfloat colored_triangle[] = {
+    // vertices      |    colors
+    0.0f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+    -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
+    0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f
+  };
+
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  GLFWwindow* w = glfwCreateWindow(800, 600, "Triangle with three colors", NULL, NULL);
+  glfwSetKeyCallback(w, key_callback);
+  glfwMakeContextCurrent(w);
+
+  glewExperimental = GL_TRUE;
+  glewIinit();
+
+  GLuint v_arr, v_buf, prog, v_shader, f_shader;
+  void* source;
+  size_t source_size;
+
+  glGenVertexArrays(1, &v_arr);
+  glGenbuffers(1, &v_buf);
+
+  glBindVertexArray(v_arr);
+  glBindBuffer(GL_ARRAY_BUFFER, v_buf);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(colored_triangle), colored_triangle, GL_STATIC_DRAW);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*)0);
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat)));
+  glEnableVertexAttribArray(0);
+  glEnableVertexAttribArray(1);
+
+  prog = glCreateProgram();
+  v_shader = glCreateShader(GL_VERTEX_SHADER);
+  f_shader = glCreateShader(GL_FRAGMENT_SHADER);
+
+  load_file("../src/shader/pas_color.vert", &source_size, &source);
+  glShaderSource(v_shader, 1, (const GLchar *const *)&source, NULL);
+  glCompileShader(v_shader);
+  check_shader(v_shader);
+  free(source);
+
   return 0;
 }
